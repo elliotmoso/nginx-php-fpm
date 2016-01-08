e57d80169ea19a1e510ec9ac73295d8294c48341
HectorRos <hectorros@gmail.com>
2016-01-08 12:06:26 +0100

    Configure NewRelic at Startup

--- start.sh
+++ start.sh
@@ -43,5 +43,22 @@ sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 #  done
 #fi
 
+# Configure NewRelic if need.
+
+NEWRELIC_LICENSE=${NEWRELIC_LICENSE:-false}
+if [ "$NEWRELIC_LICENSE" != "false" ]; then
+    sed -i "s/^;newrelic.enabled = .*/newrelic_enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^newrelic.license = .*/newrelic.license = \"${NEWRELIC_LICENSE}\"/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^;newrelic.error_collector.enabled = .*/newrelic.error_collector.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^;newrelic.transaction_tracer.enabled = .*/newrelic.transaction_tracer.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^;newrelic.transaction_tracer.threshold = .*/newrelic.transaction_tracer.threshold = \"apdex_f\"/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^;newrelic.transaction_events.enabled = .*/newrelic.transaction_events.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
+fi
+
+NEWRELIC_APP=${NEWRELIC_APP:-false}
+if [ "$NEWRELIC_APP" != "false" ]; then
+    sed -i "s/^newrelic.appname = .*/newrelic.appname = \"${NEWRELIC_APP}\"/" /etc/php5/fpm/conf.d/newrelic.ini
+fi
+
 # Start supervisord and services
 /usr/bin/supervisord -n -c /etc/supervisord.conf
