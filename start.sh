af94f433629e60e4c847c85195905eabd5a58e86
HectorRos <hectorros@gmail.com>
2016-01-08 13:02:31 +0100

    Newrelic Startup error

--- start.sh
+++ start.sh
@@ -47,17 +47,17 @@ sed -i -e "s/worker_processes 5/worker_processes $procs/" /etc/nginx/nginx.conf
 
 NEWRELIC_LICENSE=${NEWRELIC_LICENSE:-false}
 if [ "$NEWRELIC_LICENSE" != "false" ]; then
-    sed -i "s/^;newrelic.enabled = .*/newrelic_enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
-    sed -i "s/^newrelic.license = .*/newrelic.license = \"${NEWRELIC_LICENSE}\"/" /etc/php5/fpm/conf.d/newrelic.ini
-    sed -i "s/^;newrelic.error_collector.enabled = .*/newrelic.error_collector.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
-    sed -i "s/^;newrelic.transaction_tracer.enabled = .*/newrelic.transaction_tracer.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
-    sed -i "s/^;newrelic.transaction_tracer.threshold = .*/newrelic.transaction_tracer.threshold = \"apdex_f\"/" /etc/php5/fpm/conf.d/newrelic.ini
-    sed -i "s/^;newrelic.transaction_events.enabled = .*/newrelic.transaction_events.enabled = true/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^;newrelic.enabled = .*/newrelic_enabled = true/" /etc/php5/fpm/conf.d/20-newrelic.ini
+    sed -i "s/^newrelic.license = .*/newrelic.license = \"${NEWRELIC_LICENSE}\"/" /etc/php5/fpm/conf.d/20-newrelic.ini
+    sed -i "s/^;newrelic.error_collector.enabled = .*/newrelic.error_collector.enabled = true/" /etc/php5/fpm/conf.d/20-newrelic.ini
+    sed -i "s/^;newrelic.transaction_tracer.enabled = .*/newrelic.transaction_tracer.enabled = true/" /etc/php5/fpm/conf.d/20-newrelic.ini
+    sed -i "s/^;newrelic.transaction_tracer.threshold = .*/newrelic.transaction_tracer.threshold = \"apdex_f\"/" /etc/php5/fpm/conf.d/20-newrelic.ini
+    sed -i "s/^;newrelic.transaction_events.enabled = .*/newrelic.transaction_events.enabled = true/" /etc/php5/fpm/conf.d/20-newrelic.ini
 fi
 
 NEWRELIC_APP=${NEWRELIC_APP:-false}
 if [ "$NEWRELIC_APP" != "false" ]; then
-    sed -i "s/^newrelic.appname = .*/newrelic.appname = \"${NEWRELIC_APP}\"/" /etc/php5/fpm/conf.d/newrelic.ini
+    sed -i "s/^newrelic.appname = .*/newrelic.appname = \"${NEWRELIC_APP}\"/" /etc/php5/fpm/conf.d/20-newrelic.ini
 fi
 
 # Start supervisord and services
